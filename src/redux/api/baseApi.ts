4a39cb89ee8feab0fdd28ba86cbf199f48e54e42
sabrina <sabrinaahmednitu@gmail.com>
2024-10-15 23:26:03 +0600

    redux configuration done

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/blogs' }),
+  endpoints: (builder) => ({
+    getBlogs: builder.query({
+      query: () => "/blogs",
+    }),
+  }),
+});
+// auto-generated based on the defined endpoints
+export const { useGetBlogsQuery } = baseApi;
